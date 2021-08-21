e21a4285f25879bb1e2e29e7ddd2ddf76b74477c
gleissonneves <gleissonneves2017@gmail.com>
2021-08-21 12:33:53 -0300

    add http post request methods

--- dist/httpRequestAxios/httpRequestAxios.js
+++ dist/httpRequestAxios/httpRequestAxios.js
@@ -2,7 +2,7 @@ import * as axios from 'axios';
 import { getMessageStatusCode } from '../../libs/helpers/verifyCodeHttp';
 
 /**
- * Get method
+ * Get resquest method
  *
  * @param string url Endpoint for resource
  * @param null|function callback Function to be performed on success
@@ -33,7 +33,36 @@ export function get(url, callback = null) {
  * Get request method with parameter passing
  * 
  * @param string url Endpoint for resource
- * @param object|string data Data passed by the request body
+ * @param object data Data passed by the request body
+ * @param null|function callback Function to be performed on successs
+ */
+export function getParam(url, data, callback = null) {
+    axios
+        .get(url, {data})
+        .then((response) => {
+            return callback == null ? response : callback(response);
+        })
+        .catch((error) => {
+            if (error.response) {
+                return {
+                    data: error.response.data,
+                    code: error.response.status,
+                    message: getMessageStatusCode(error.response.status),
+                    statusText: error.response.statusText,
+                }
+            } else {
+                return {
+                    error: error.message,
+                }
+            }
+        });
+}
+
+/**
+ * POST request method
+ *
+ * @param string url Endpoint for resource
+ * @param object data Data passed by the request body
  * @param null|function callback Function to be performed on successs
  */
 export function getParam(url, data, callback = null) {
